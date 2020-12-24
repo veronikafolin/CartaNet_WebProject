d902d3ffa387ec2cd445e39dbd8f9dab3a00ea5a
Davide Domini <davide.domini@studio.unibo.it>
2020-12-24 10:20:23 +0100

    Modificata login form

--- template/login-form.php
+++ template/login-form.php
@@ -1,15 +1,15 @@
 <form action="#" method="POST">
-    <div class="form-group" style="text-align: center;">
+    <div class="d-flex justify-content-center col-xl-12">
         <h2 >Login</h2>
         <?php if(isset($templateParams["errorelogin"])): ?>
         <p><?php echo $templateParams["errorelogin"]; ?></p>
         <?php endif; ?>
         <ul style="list-style:none">
             <li>
-                <label for="username">Username:</label><input class="form-control form-control-sm col-xl-4" type="text" id="username" name="username" />
+                <label for="username">Username:</label><input class="form-control form-control-sm col-xl-8" type="text" id="username" name="username" />
             </li>
             <li>
-                <label for="password">Password:</label><input class="form-control form-control-sm col-xl-4" type="password" id="password" name="password" />
+                <label for="password">Password:</label><input class="form-control form-control-sm col-xl-8" type="password" id="password" name="password" />
             </li>
             <li>
                 <input type="submit" name="submit" value="Invia" />
