1923804ea4659b01e64c61989c5e7c2fedeea556
veronikafolin <veronika.folin@studio.unibo.it>
2021-01-02 16:48:57 +0100

    updates css

--- template/login-form.php
+++ template/login-form.php
@@ -1,21 +1,22 @@
-
- <div class="row justify-content-center align-items-center" style="margin: 20px 0">
-    <form action="#" method="post" style="margin-top: 20px"> 
-        <h2 style="font-size: 16pt; align: center">Accedi all'area riservata</h2>
-        <?php if(isset($templateParams["errorelogin"])): ?>
-            <p><?php echo $templateParams["errorelogin"]; ?></p>
-        <?php endif; ?>
-        <div class="form-group col-10">
-            <label for="username">Email:</label>
-            <input class="form-control" type="email" id="username" name="username" required/>
-        </div>
-        <div class="form-group col-10">
-            <label for="password">Password:</label>
-            <input class="form-control" type="password" id="password" name="password" required/>
-        </div>
-        <div class="form-group col-4 " >
-        <input class="btn btn-primary" type="submit" name="submit" value="Login"/>
-        </div>
-        <a href="signup.php">Non sei ancora registrato? Clicca qui</a>
-    </form>
-</div>
+<section>
+    <h2>Accedi all'area riservata</h2>
+    <div class="row justify-content-center align-items-center">
+        <form action="#" method="post"> 
+            <?php if(isset($templateParams["errorelogin"])): ?>
+                <p><?php echo $templateParams["errorelogin"]; ?></p>
+            <?php endif; ?>
+            <div class="form-group col-10">
+                <label for="username">Email:</label>
+                <input class="form-control" type="email" id="username" name="username" required/>
+            </div>
+            <div class="form-group col-10">
+                <label for="password">Password:</label>
+                <input class="form-control" type="password" id="password" name="password" required/>
+            </div>
+            <div class="form-group col-4 " >
+            <input class="btn btn-primary" type="submit" name="submit" value="Login"/>
+            </div>
+            <a href="signup.php">Non sei ancora registrato? Clicca qui</a>
+        </form>
+    </div>
+</section>
--- template/mieiOrdini.php
+++ template/mieiOrdini.php
@@ -1,10 +1,9 @@
-
-<section style="margin: 80px; clear: both;">
-    <center><h2>I miei Ordini</h2></center>
+<section>
+    <h2>I miei Ordini</h2>
     <div class="container-fluid">
         <?php foreach($templateParams["mieiOrdini"] as $ordine): ?>
-             <div class="row" style="width: 100%; border-bottom: 1px solid black; padding: 10px 0px;"> 
-                 <div class="col-sm-3 col-xl-6" style="padding: 0px">
+             <div class="row ordine"> 
+                 <div class="col-12 col-xl-8">
                     <h3> Id Ordine: <?php echo $ordine["IdOrdine"]; ?> </h3>
                     <p>
                     Data Ordine: <?php echo $ordine["DataOrdine"]; ?> </br>
@@ -12,7 +11,7 @@
                     Totale Ordine: <?php echo $ordine["Totale"]." €" ; ?>
                     </p> 
                  </div>
-                 <div class="col-sm-3 col-xl-6" style="padding: 0px; align-items: center; justify-content: center; display: flex;">
+                 <div class="col-12 col-xl-4">
                     <a href="dettaglioOrdine.php?IdOrdine=<?php echo $ordine["IdOrdine"];  ?>">
                     <button type="button" class="btn btn-primary">Vedi dettagli ordine</button>
                     </a>
