54ac0da52e04099921afbd9d5ca1e8c6bd7aa7e9
veronikafolin <veronika.folin@studio.unibo.it>
2021-01-04 21:22:59 +0100

    updates css

--- template/prodotto.php
+++ template/prodotto.php
@@ -2,9 +2,11 @@
 $prodotto = $templateParams["prodotto"];
 ?>
 
-<section class="prodotto">
+<section>
     <div class="container-fluid">
-    <h2> <?php echo $prodotto["NomeProdotto"]; ?> </h2>
+        <div class="nomeProdotto row justify-content-center align-items-center">
+            <h2> <?php echo $prodotto["NomeProdotto"]; ?> </h2>
+        </div>
         <div class="row"> 
             <img class = "col-12 col-xl-4" src="<?php echo ".".$prodotto["Immagine"];?>" alt="<?php echo "Immagine di ".$prodotto["NomeProdotto"];?>"/>
             <div class= "col-12 col-xl-8">
