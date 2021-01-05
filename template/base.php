603bd7508c03599311791db83778978600abebc3
Davide Domini <davide.domini@studio.unibo.it>
2021-01-05 22:31:03 +0100

    aggiunto meta charset in base

--- template/base.php
+++ template/base.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="it">
 <head>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $templateParams["titolo"]; ?></title>
     <script src="js/functions.js"></script>
